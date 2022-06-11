e082965bfde0a3c04ce8bc641d4e13244c4cc6ea
oumburs9 <oumburs9mry@gmail.com>
2022-06-11 07:30:36 +0300

    adding getFileContent and some lines of code in init fun

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from OpenGL.GL import *
 from pygame.locals import *
 from OpenGL.GL.shaders import *
 import numpy as np
-# import os
+import os
+
+def getFileContents(filename):
+    p = os.path.join(os.getcwd(), "shaders", filename)
+    print(p)
+    return open(p, 'r').read()
 
 def init():
     global vao, vbo, ebo, shader
@@ -50,7 +55,64 @@ def init():
             4, 0, 3,
 
         ], dtype=np.uint32)
-        
+
+    vertexShader = compileShader(getFileContents(
+        "triangle.vertex.shader"), GL_VERTEX_SHADER)
+    fragmentShader = compileShader(getFileContents(
+        "triangle.fragment.shader"), GL_FRAGMENT_SHADER)
+    shader = glCreateProgram()
+
+
+    shader = glCreateProgram()
+    glAttachShader(shader, vertexShader)
+    glAttachShader(shader, fragmentShader)
+    glLinkProgram(shader)
+
+    # Create Buffer object in
+    VBO = glGenBuffers(1)
+
+    # Create EBO
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
+
+    # Bind the buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+    # get the position from  shader
+    position = glGetAttribLocation(shader, 'position')
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, vertices.itemsize * 6, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(position)
+
+    # get the color from  shader
+    color = glGetAttribLocation(shader, 'color')
+    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, vertices.itemsize * 6, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(color)
+
+    glUseProgram(shader)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    # glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+    # glClearColor(0.0, 1.0, 0.0, 1.0)
+    
+
+
+def draw(count):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    # enable depth test
+    glEnable(GL_DEPTH_TEST)
+    # apply the depth checking function
+    glDepthFunc(GL_LESS)
+    # Draw Triangle
+
+    transk = glGetUniformLocation(shader, 'transform')
+    # print(transform.rotationMatrix(30))
+
+    # # glUniformMatrix4fv(transk, 1, GL_FALSE, rotationMatrix(count))
+
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, None)
+
 def main():
     init()
     count = 0
@@ -60,7 +122,7 @@ def main():
                 pygame.quit()
                 quit()
         if count == 360: count = 0
-        # draw(count)
+        draw(count)
         count += 1
         glRotate(1, 3, 1, 3)
         pygame.display.flip()
