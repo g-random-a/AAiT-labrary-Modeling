8159d276d067a1d76046b7b8d2e554c568cb503c
oumburs9 <oumburs9mry@gmail.com>
2022-06-13 11:57:32 +0300

    fished main_without grass

--- main_with_out_grass_fence.py
+++ main_with_out_grass_fence.py
@@ -10,7 +10,63 @@ from OpenGL.GL.shaders import *
 import os 
 from PIL import * 
  
-
+def getFileContents(filename): 
+    p = os.path.join(os.getcwd(), "shaders", filename) 
+    return open(p, 'r').read() 
+ 
+pygame.init() 
+cam = Camera() 
+vport = (800,600) 
+height = vport[0]/2 
+width = vport[1]/2 
+pygame.display.set_mode(vport, OPENGL | DOUBLEBUF) 
+ 
+vertexes = np.array([ 
+     
+]) #to be filled  
+vertexShader = compileShader(getFileContents( 
+        "triangle.vertex.shader"), GL_VERTEX_SHADER) 
+fragmentShader = compileShader(getFileContents( 
+    "triangle.fragment.shader"), GL_FRAGMENT_SHADER) 
+ 
+program = glCreateProgram() 
+glAttachShader(program, vertexShader) 
+glAttachShader(program, fragmentShader) 
+glLinkProgram(program) 
+ 
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0)) 
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0)) 
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0)) 
+glEnable(GL_LIGHT0) 
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
+                          8 * vertexes.itemsize, ctypes.c_void_p(0)) 
+glEnableVertexAttribArray(0) 
+ 
+# colorLocation = glGetAttribLocation(program, "color"); 
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 
+                        8 * vertexes.itemsize, ctypes.c_void_p(12)) 
+glEnableVertexAttribArray(1) 
+ 
+# texLocation = glGetAttribLocation(program, "texCoord"); 
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,  
+    8*vertexes.itemsize, ctypes.c_void_p(24)) 
+glEnableVertexAttribArray(2) 
+glEnable(GL_LIGHTING) 
+glEnable(GL_COLOR_MATERIAL) 
+glEnable(GL_DEPTH_TEST) 
+glShadeModel(GL_SMOOTH)  
+ 
+obj = OBJ("mesh/of3.obj", swapyz=True) 
+obj.generate() 
+ 
+clock = pygame.time.Clock() 
+ 
+glMatrixMode(GL_PROJECTION) 
+glLoadIdentity() 
+width, height = vport 
+gluPerspective(90.0, width/float(height), 1, 100.0) 
+glEnable(GL_DEPTH_TEST) 
+glMatrixMode(GL_MODELVIEW) 
  
  
 while 1: 
