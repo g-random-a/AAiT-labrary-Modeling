9dbd87e6efaace25e5fb3f8e1c286bc8481c6194
habib-gm <habib.gemmech@gmail.com>
2022-06-13 12:07:12 +0300

    obj loader imported

---
+++ loader/objloader.py
@@ -0,0 +1,92 @@
+import os
+import pygame
+from OpenGL.GL import *
+class OBJ:
+    generate_on_init = True
+    def loadTexture(self, imagefile):
+        surf = pygame.image.load(imagefile)
+        image = pygame.image.tostring(surf, 'RGBA', 1)
+        ix, iy = surf.get_rect().size
+        texid = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texid)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+        return texid
+
+    def loadMaterial(self, filename):
+        contents = {}
+        mtl = None
+        dirname = os.path.dirname(filename)
+
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'newmtl':
+                mtl = contents[values[1]] = {}
+            elif mtl is None:
+                raise ValueError("mtl file doesn't start with newmtl stmt")
+            elif values[0] == 'map_Kd':
+                mtl[values[0]] = values[1]
+                imagefile = os.path.join(dirname, mtl['map_Kd'])
+                mtl['texture_Kd'] = cls.loadTexture(imagefile)
+            else:
+                mtl[values[0]] = list(map(float, values[1:]))
+        return contents
+
+    def __init__(self, filename, swapyz=False):
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        self.gl_list = 0
+        dirname = os.path.dirname(filename)
+
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'v':
+                v = list(map(float, values[1:4]))
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = list(map(float, values[1:4]))
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(list(map(float, values[1:3])))
+            elif values[0] in ('usemtl', 'usemat'):
+                material = values[1]
+            elif values[0] == 'mtllib':
+                self.mtl = self.loadMaterial(os.path.join(dirname, values[1]))
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append((face, norms, texcoords, material))
+        if self.generate_on_init:
+            self.generate()
+
+    
+
+    def render(self):
+        glCallList(self.gl_list)
+
+    def free(self):
+        glDeleteLists([self.gl_list])
