c67feaec413fccd0999e5919fda8d648c866857f
oumburs9 <oumburs9mry@gmail.com>
2022-06-18 23:57:06 +0300

    modifying main without grass fence

--- main_with_out_grass_fence.py
+++ main_with_out_grass_fence.py
@@ -21,19 +21,18 @@ glEnable(GL_COLOR_MATERIAL)
 glEnable(GL_DEPTH_TEST) 
 glShadeModel(GL_SMOOTH) 
  
-obj = OBJ("mesh/of3.obj", swapyz=True) 
-obj.generate() 
+
+obj = OBJ("mesh/of3.obj")  
  
 clock = pygame.time.Clock() 
- 
 glMatrixMode(GL_PROJECTION) 
 glLoadIdentity() 
-width, height = vport 
+width, height = (800,600) 
 gluPerspective(90.0, width/float(height), 1, 100.0) 
 glEnable(GL_DEPTH_TEST) 
 glMatrixMode(GL_MODELVIEW) 
  
- 
+glClearColor(1,1,1,1)
 while 1: 
     clock.tick(30) 
     for e in pygame.event.get(): 
@@ -44,9 +43,9 @@ while 1:
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) 
     glLoadIdentity() 
  
-    glTranslate(cam.transX/20., cam.transY/20., - cam.zpos) 
+    glTranslate(cam.transX/10., cam.transY/10.,cam.transY/10. - cam.zpos) 
     glRotate(cam.rotx, 1, 0, 0) 
-    glRotate(cam.rotx, 0, 1, 0) 
+    glRotate(cam.roty, 0, 1, 0) 
     glScale(cam.sx, cam.sy, cam.sz) 
     obj.render() 
  
