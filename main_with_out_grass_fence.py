b1ab833545630239ecc2d52c3a4af88732018aa3
oumburs9 <oumburs9mry@gmail.com>
2022-06-18 11:55:41 +0300

    modifying main without grass

--- main_with_out_grass_fence.py
+++ main_with_out_grass_fence.py
@@ -9,52 +9,17 @@ import numpy as np
 from OpenGL.GL.shaders import * 
 import os 
 from PIL import * 
- 
-def getFileContents(filename): 
-    p = os.path.join(os.getcwd(), "shaders", filename) 
-    return open(p, 'r').read() 
- 
 pygame.init() 
 cam = Camera() 
-vport = (800,600) 
-height = vport[0]/2 
-width = vport[1]/2 
-pygame.display.set_mode(vport, OPENGL | DOUBLEBUF) 
- 
-vertexes = np.array([ 
-     
-]) #to be filled  
-vertexShader = compileShader(getFileContents( 
-        "triangle.vertex.shader"), GL_VERTEX_SHADER) 
-fragmentShader = compileShader(getFileContents( 
-    "triangle.fragment.shader"), GL_FRAGMENT_SHADER) 
- 
-program = glCreateProgram() 
-glAttachShader(program, vertexShader) 
-glAttachShader(program, fragmentShader) 
-glLinkProgram(program) 
- 
+pygame.display.set_mode((800,600) , OPENGL | DOUBLEBUF) 
 glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0)) 
 glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0)) 
 glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0)) 
 glEnable(GL_LIGHT0) 
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
-                          8 * vertexes.itemsize, ctypes.c_void_p(0)) 
-glEnableVertexAttribArray(0) 
- 
-# colorLocation = glGetAttribLocation(program, "color"); 
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 
-                        8 * vertexes.itemsize, ctypes.c_void_p(12)) 
-glEnableVertexAttribArray(1) 
- 
-# texLocation = glGetAttribLocation(program, "texCoord"); 
-glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,  
-    8*vertexes.itemsize, ctypes.c_void_p(24)) 
-glEnableVertexAttribArray(2) 
 glEnable(GL_LIGHTING) 
 glEnable(GL_COLOR_MATERIAL) 
 glEnable(GL_DEPTH_TEST) 
-glShadeModel(GL_SMOOTH)  
+glShadeModel(GL_SMOOTH) 
  
 obj = OBJ("mesh/of3.obj", swapyz=True) 
 obj.generate() 
